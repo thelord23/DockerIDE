40c71f6e9f7ee724773eb609ab91ddc1f6336b38
thelord23 <levyran@gmail.com>
2019-06-08 15:05:26 +0000

    ide image build

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DockerIDE
+This image contain IDE enviorment with vim git and more.
+The file ide.sh contains and exmample of how to run this container
---
+++ ide.sh
@@ -0,0 +1,3 @@
+docker run --rm -v workspace:/home/workspace/ -v //var/run/docker.sock:/var/run/docker.sock -p 22:22 -d --name ide thelord23/ide:latest 
+
+
